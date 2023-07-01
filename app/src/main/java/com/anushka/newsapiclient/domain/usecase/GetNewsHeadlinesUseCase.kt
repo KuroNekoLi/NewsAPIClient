a82a7cf8cafed7f25689a081393760cf9cc0d1af
KuroNekoLi <127180158+KuroNekoLi@users.noreply.github.com>
2023-07-01 23:04:09 +0800

    View Model and View Model Factory

--- app/src/main/java/com/anushka/newsapiclient/domain/usecase/GetNewsHeadlinesUseCase.kt
+++ app/src/main/java/com/anushka/newsapiclient/domain/usecase/GetNewsHeadlinesUseCase.kt
@@ -5,7 +5,7 @@ import com.anushka.newsapiclient.data.util.Resource
 import com.anushka.newsapiclient.domain.repository.NewsRepository
 
 class GetNewsHeadlinesUseCase(private val newsRepository: NewsRepository) {
-    suspend fun execute(): Resource<APIResponse> {
-        return newsRepository.getNewsHeadlines()
+    suspend fun execute(country:String,page:Int): Resource<APIResponse> {
+        return newsRepository.getNewsHeadlines(country,page)
     }
 }
